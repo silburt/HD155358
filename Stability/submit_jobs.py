c2e9288d2ef8e994c9e8c028a89ddb28356f0d45
silburt <silburt@astro.utoronto.ca>
2017-03-16 17:14:18 -0400

    work

--- Stability/submit_jobs.py
+++ Stability/submit_jobs.py
@@ -5,11 +5,9 @@ jobs_dir = 'jobs/'
 
 os.system('make clean && make')
 os.system('mv rebound %s.'%jobs_dir)
-os.system('cd %s'%jobs_dir)
+os.chdir(jobs_dir)
 
-files = glob.glob()
+files = glob.glob('tmax*')
 for f in files:
-    #job_name = f.split(jobs_dir)[1]
-    #os.system('mv %s %s'%(f, job_name))
-    os.system('qsub %s'%f)           #submit job in home directory
-    #os.system('mv %s %s'%(job_name,f))
+    os.system('qsub %s'%f)
+
