06d87335bc4b12e7412013e6a8eb7a2ce2ac211a
silburt <silburt@astro.utoronto.ca>
2017-03-16 17:28:42 -0400

    changes

--- Stability/submit_jobs.py
+++ Stability/submit_jobs.py
@@ -4,10 +4,12 @@ import glob
 jobs_dir = 'jobs/'
 
 os.system('make clean && make')
-os.system('mv rebound %s.'%jobs_dir)
-os.chdir(jobs_dir)
+#os.system('mv rebound %s.'%jobs_dir)
+#os.chdir(jobs_dir)
 
-files = glob.glob('tmax*')
+files = glob.glob('%s/*'%jobs_dir)
 for f in files:
-    os.system('qsub %s'%f)
-
+    job_name = f.split(jobs_dir)[1]
+    os.system('mv %s %s'%(f, job_name))
+    os.system('qsub %s'%job_name)
+    os.system('mv %s %s'%(job_name,f))
