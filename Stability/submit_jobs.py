8e9b15fb1a2f3361887617d82d8e7758368f0d66
silburt <silburt@astro.utoronto.ca>
2017-03-27 11:39:04 -0400

    changes

--- Stability/submit_jobs.py
+++ Stability/submit_jobs.py
@@ -23,7 +23,7 @@ for f in files:
     csv = 'output/%s.csv'%job_name
     if os.path.exists(csv):
         time, dE, stable, a1, e1, l1, w1, M1, a2, e2, l2, w2, M2, phi1, phi2, phi3 = np.loadtxt(open(csv, 'r'), delimiter=',', unpack=True)
-        if stable[-1] == 1 and time[-1]<6.28e9:
+        if stable[-1] == 1 and time[-1]<6.26e9:
             print 'restarting job: %s'%job_name
             submit_job(f, job_name)
             Njobs += 1
